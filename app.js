4c88207746ae96f97ccdacb93b60bb3c922d5bc8
reid <reidvrana93@gmail.com>
2020-02-11 00:56:37 +0000

    add style

--- app.js
+++ app.js
@@ -59,3 +59,7 @@ app.use("/courses/:id/comments", commentRoutes);
 
 
 app.listen(process.env.PORT||5000, process.env.IP);
+
+// app.listen(3000, function() { 
+// 	console.log('Server is running!'); 
+// });
