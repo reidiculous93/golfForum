347ed66f39a92b279d90a704dbb50f167c76b085
reid <reidvrana93@gmail.com>
2020-02-11 00:31:31 +0000

    add heroku port

--- app.js
+++ app.js
@@ -58,6 +58,4 @@ app.use("/courses", coursesRoutes);
 app.use("/courses/:id/comments", commentRoutes);
 
 
-app.listen(3000, function() { 
-	console.log('Server is running!'); 
-});
+app.listen(process.env.PORT||5000, process.env.IP);
